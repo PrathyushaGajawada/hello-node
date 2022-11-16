ac36347d42c74d0fd2c490a55cf9c5a7a6a2b745
PrathyushaGajawada <118366440+PrathyushaGajawada@users.noreply.github.com>
2022-11-17 01:04:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const http = require("http");
-const fs = require("fs");
-const server = http.createServer((req,res) => {
-    fs.readFile("sample.txt",(err,data) => {
-        res.end(data);
-    })
-}
-);
-server.listen(3000);
+const hello = () => {
+    console.log("Hello Node.js!");
+
+};
+hello();
